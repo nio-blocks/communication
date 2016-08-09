47524debee06ba46c7c32a4cd915bc5f948df05c
Franky Martin <fmartin@neutral.io>
2016-08-09 15:26:58 -0400

    NIO-945, changes to blocks to accommodate new way to define topic

--- tests/test_pub_sub.py
+++ tests/test_pub_sub.py
@@ -1,10 +1,45 @@
-from ..publisher import Publisher
-from ..subscriber import Subscriber
+from nio import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
+
+from ..publisher import Publisher as Publisher
+from ..subscriber import Subscriber as Subscriber
 
 
 class TestPubSub(NIOBlockTestCase):
 
-    def test_pub_sub(self):
-        """ TODO: Implement tests with mocking """
-        pass
+    def get_test_modules(self):
+        return super().get_test_modules() | {'communication'}
+
+    def test_publisher(self):
+        publisher = Publisher()
+
+        # assert that it needs to be configured
+        with self.assertRaises(AttributeError):
+            publisher.process_signals([Signal()])
+
+        topic = "test_topic"
+        self.configure_block(publisher, {"topic": topic})
+        # assert that topic property value is now available
+        self.assertEqual(publisher.topic(), topic)
+
+        publisher.start()
+        # now it can process signals
+        publisher.process_signals([Signal()])
+        publisher.stop()
+
+    def test_subscriber(self):
+        subscriber = Subscriber()
+
+        # assert that it needs to be configured
+        with self.assertRaises(AttributeError):
+            subscriber.process_signals([Signal()])
+
+        topic = "test_topic"
+        self.configure_block(subscriber, {"topic": topic})
+        # assert that topic property value is now available
+        self.assertEqual(subscriber.topic(), topic)
+
+        subscriber.start()
+        # now it can process signals
+        subscriber.process_signals([Signal()])
+        subscriber.stop()
