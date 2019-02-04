7740708543b003b5ac7a91897733e2777468b2f5
J. Holmes <jholmes@n.io>
2019-02-04 16:06:42 -0700

    refactor Job _cleanup
    Refactoring away from a single _cleanup job to one TTL job per topic. New signals reschedule the job for a new TTL window. Expired jobs tear themselves down and out of the dicationary.

--- dynamic_publisher.py
+++ dynamic_publisher.py
@@ -10,6 +10,14 @@ from nio.properties import StringProperty, TimeDeltaProperty, VersionProperty
 
 from .connectivity import PubSubConnectivity
 
+class keydefaultdict(defaultdict):
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError( key )
+        else:
+            ret = self[key] = self.default_factory(key)
+            return ret
+
 class DynamicPublisher(PubSubConnectivity, TerminatorBlock):
     version = VersionProperty("0.1.0")
     topic = StringProperty(title="Topic", default="")
@@ -18,46 +26,16 @@ class DynamicPublisher(PubSubConnectivity, TerminatorBlock):
                             advanced=True,
                             order=0,
                             default=dict(seconds=10))
-    interval = TimeDeltaProperty(title="Teardown Interval",
-                                 advanced=True,
-                                 default=dict(seconds=1),
-                                 order=1)
     def __init__(self):
         super().__init__()
-        self._cache = dict()
+        self._cache = keydefaultdict(lambda topic: (self._create_publisher(topic), None))
         self._cache_lock = Lock()
-        self._job = None
-
-    def start(self):
-        self._job = Job(
-            self._cleanup,
-            self.interval(),
-            True,
-        )
-
-    def _cleanup(self):
-        now = monotonic()
-        ttl = self.ttl().total_seconds()
-
-        with self._cache_lock:
-            expired_topics = []
-            for topic in self._cache:
-                _, prev = self._cache[topic]
-
-                if now - prev > ttl:
-                    expired_topics.append(topic)
-
-            for topic in expired_topics:
-                self.logger.info('removing expired publisher for "{}"'.format(topic))
-                pub, _ = self._cache[topic]
-                pub.close()
-                del self._cache[topic]
-
 
     def process_signals(self, in_signals):
         """ Publish each list of signals """
-
+        ttl = self.ttl()
         groups = defaultdict(list)
+
         for signal in in_signals:
             try:
                 topic = self.topic(signal)
@@ -67,38 +45,46 @@ class DynamicPublisher(PubSubConnectivity, TerminatorBlock):
             groups[topic].append(signal)
 
 
-        for topic in groups:
+        for topic, out_signals in groups.items():
             try:
-                out_signals = groups[topic]
-                self._get_publisher(topic).send(out_signals)
+                self._get_publisher(topic, ttl).send(out_signals)
             except PublisherError:  # pragma no cover
                 self.logger.exception('Error publishing {:n} signals to "{}"'.format(len(out_signals), topic))
 
-    def _get_publisher(self, topic):
-        now = monotonic()
-
+    def _cleanup(self, topic):
         with self._cache_lock:
-            if topic in self._cache:
-                publisher, _ = self._cache[topic]
-                self._cache[topic] = (publisher, now)
-                return publisher
+            self.logger.info('removing expired publisher for "{}"'.format(topic))
+            pub, _ = self._cache.pop(topic)
+            pub.close()
 
-            self.logger.info('creating new publisher for "{}"'.format(topic))
-            publisher = NioPublisher(topic=topic)
+    def _create_publisher(self, topic):
+        self.logger.info('creating new publisher for "{}"'.format(topic))
+        publisher = NioPublisher(topic=topic)
 
-            try:
-                publisher.open(on_connected=self.conn_on_connected,
-                               on_disconnected=self.conn_on_disconnected)
-            except TypeError as e:
-                self.logger.warning(
-                    'Connecting to an outdated communication module')
-                # try previous interface
-                publisher.open()
-                # no need to configure connectivity if not supported
-                return publisher
-
-            self.conn_configure(publisher.is_connected)
-            self._cache[topic] = (publisher, now)
+        try:
+            publisher.open(on_connected=self.conn_on_connected,
+                           on_disconnected=self.conn_on_disconnected)
+        except TypeError as e:
+            self.logger.warning(
+                'Connecting to an outdated communication module')
+            # try previous interface
+            publisher.open()
+            # no need to configure connectivity if not supported
             return publisher
 
+        self.conn_configure(publisher.is_connected)
+        return publisher
 
+    def _get_publisher(self, topic, ttl):
+        with self._cache_lock:
+            publisher, prev_job = self._cache[topic]
+            if prev_job is not None:
+                prev_job.cancel()
+
+            self._cache[topic] = (publisher, Job(
+                self._cleanup,
+                ttl,
+                False,
+                topic))
+
+            return publisher
