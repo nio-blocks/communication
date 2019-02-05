e56b835e6b3b233953898504221665d7df2b970d
J. Holmes <jholmes@n.io>
2019-02-05 10:41:51 -0700

    whitespace and cleanup

--- dynamic_publisher.py
+++ dynamic_publisher.py
@@ -1,8 +1,7 @@
 from collections import defaultdict
 from threading import Lock
-from time import monotonic
 
-from nio import TerminatorBlock, Block
+from nio import TerminatorBlock
 from nio.modules.communication.publisher import Publisher as NioPublisher
 from nio.modules.communication.publisher import PublisherError
 from nio.modules.scheduler import Job
@@ -10,13 +9,15 @@ from nio.properties import StringProperty, TimeDeltaProperty, VersionProperty
 
 from .connectivity import PubSubConnectivity
 
+
 class keydefaultdict(defaultdict):
     def __missing__(self, key):
         if self.default_factory is None:
-            raise KeyError( key )
+            raise KeyError(key)
         else:
             ret = self[key] = self.default_factory(key)
             return ret
+
 
 class DynamicPublisher(PubSubConnectivity, TerminatorBlock):
     version = VersionProperty("0.1.0")
@@ -43,7 +44,6 @@ class DynamicPublisher(PubSubConnectivity, TerminatorBlock):
                 self.logger.error('topic expression failed, ignoring signal')
                 continue
             groups[topic].append(signal)
-
 
         for topic, out_signals in groups.items():
             try:
